d1c3746e5eb77416e4fd5652651407b25fd1cee7
outsung <40460655+outsung@users.noreply.github.com>
2019-09-05 15:38:53 +0900

    Update 0813.js

--- script/0813.js
+++ script/0813.js
@@ -12,40 +12,42 @@ let square = {
 let object = {
     position : 0,
     wall : [{
-
     }]
 }
 */
 
-// 내장 함수
+//------------내장 함수-------------------
+
 //사이즈 변경하면 실행하는 함수
 function resize() {
-  if(unNameVariable === true){
+  if(oGame.vStart === true){
+      
     let hUserScreen = document.getElementById("userScreen");
+      
     console.log('Resizing...')
 
-    userScreen.width = window.innerWidth;
-    userScreen.height = window.innerHeight;
+    //userScreen.width = window.innerWidth;
+    //userScreen.height = window.innerHeight;
 
-    hUserScreen.style.width = userScreen.width + "px";
-    hUserScreen.style.height = userScreen.height + "px";
+    hUserScreen.style.width = window.innerWidth + "px";
+    hUserScreen.style.height = window.innerHeight + "px";
 
 
 
-    let hPlayer = document.getElementById("player");
-    hPlayer.style.marginLeft = userScreen.width / 2 - 5 + "px";
-    hPlayer.style.marginTop = userScreen.height / 2 - 5 + "px";
+    //let hPlayer = document.getElementById("player");
+    //hPlayer.style.marginLeft = userScreen.width / 2 - 5 + "px";
+    //hPlayer.style.marginTop = userScreen.height / 2 - 5 + "px";
   }
 };
 
 //키 입력 시 실행하는 함수
 function keyPress(){
-  if(unNameVariable === true){
+  if(oPlay.vStart === true){
     let downKeyCode = event.keyCode;
     //console.log(downKeyCode);
 
-    if (downKeyCode in keyCode){
-      keyCode[downKeyCode] = 1;
+    if (downKeyCode in oPlay.keyCode){
+      oPlay.keyCode[downKeyCode] = true;
     }
     else{
       event.returnValue = false; // 브라우저 기능 키 무효화
@@ -55,16 +57,25 @@ function keyPress(){
 }
 
 function keyUp(){
-  if(unNameVariable === true){
+  if(oPlay.vStart === true){
     let upKeyCode = event.keyCode;
     //console.log(upKeyCode);
 
-    if (upKeyCode in keyCode){
-      keyCode[upKeyCode] = 0;
+    if (upKeyCode in oPlay.keyCode){
+      oPlay.keyCode[upKeyCode] = false;
     }
     //console.log(keyCode);
   }
 }
+
+
+function isKeyDown(key){
+  if(oPlay.vStart === true) return keyCode[key];   
+  else return -1;
+}
+
+//----------------------------------------
+
 
 function getRandomInt(min, max){
   return Math.floor(Math.random() * (max - min)) + min;
@@ -74,11 +85,46 @@ function getRandomBool(threshold){
     return threshold >= getRandomInt(1,101);
 }
 
-function fplay(){
-  this.vStart
+
+
+
+//--------------상황------------
+
+function fGame(){ // 게임 시작
+    
+    // 마우스 위치?
+    this.vStart = true;
 }
 
 
+function fPlay(){ // 게임 플레이
+  
+    // 마우스 고정 & 마우스
+    this.vStart = true;
+    
+    //https://blog.outsider.ne.kr/322
+    this.keyCode = {
+    27 : false,
+    87 : false,
+    65 : false,
+    83 : false,
+    68 : false,
+    122 : false,
+    123 : false
+    };
+  
+}
+
+function fUi(){ // 게임 ui
+    
+    // 마우스 not 고정 &  
+    this.vStart = false;
+}
+
+//-------------------------------
+
+
+//----------맵-----------------
 function fMap(width, height, initV){
     this.width = width;
     this.height = height;
@@ -162,13 +208,14 @@ function fMap(width, height, initV){
             else return vNeighbors;
         }
 
-
-        let ruleB = rule.split("/").[0].split;
-        let ruleS = rule.split("/").[0].split;
+        
+        let ruleB = rule.split("/")[0].split;
+        let ruleS = rule.split("/")[0].split;
 
         for(let i in ruleB)
-        {
-            if(typeof(i =* 1) === "number") B[i] = true;
+        {   
+            console.log(i);
+            if(typeof(i *= 1) === "number") B[i] = true;
             else{
                 if(i !== "B"){
                     console.log("fMapSellularAutomata-[B]error-!");
@@ -178,7 +225,7 @@ function fMap(width, height, initV){
         }
         for(let i in ruleS)
         {
-            if(typeof(i =* 1) === "number") S[i] = true;
+            if(typeof(i *= 1) === "number") S[i] = true;
             else{
                 if(i !== "S"){
                     console.log("fMapSellularAutomata-[S]error-!");
@@ -221,12 +268,93 @@ function fMap(width, height, initV){
 
 
     this.fDrawMap = function(){
-
+        
+        // 맵 그리기
+        
+        //-----------블럭-----------
+        let alive = function(x,y){
+            
+            const hMap = document.getElementById("map");
+            const hDraw = hMap.getContext("2d");
+            
+            
+            const width = 10;
+            const height = 10;
+            const style = "rgb(200,0,0)";
+            
+            
+            hDraw.fillStyle = style;
+            hDraw.fillRect (x, y, width, height);
+            
+        } 
+        let dead = function(x,y){
+            
+            const hMap = document.getElementById("map");
+            const hDraw = hMap.getContext("2d");
+            
+            
+            const width = 10;
+            const height = 10;
+            const style = "rgb(200,200,0)";
+            
+            
+            hDraw.fillStyle = style;
+            hDraw.fillRect (x, y, width, height);
+            
+        }
+        //-------------------------
+        
+        
+        for(let j in this.aMap){
+            for(let i in j){
+                if(this.aMap[j][i] === true) alive(i*10, j*10);
+                else dead(i*10, j*10);
+            }
+        }
+        
+        
     }
+}
+//-------------------------------
+
+
+
+
+//-------------------main-----------
+
+function main(){
+
+    let oGame = new fGame();
+
+    let oPlay = new fPlay();
+
+    //let oUi = new fUi();
+    
+    let oMap = new fMap(10,10);
+    
+    oMap.fDrawMap();
+
+    //vMap.fMapSellularAutomata("B23/S45");
+    
+    let iRule = "B23/S45"
+    
+    while(isKeyDown(27) !== true){
+          
+        if(isKeyDown(65) === true){
+            oMap.fMapSellularAutomata(iRule);
+            oMap.fDrawMap();
+        }
+    }
+    
 }
 
 
-let vMap = new fMap(10,10);
-console.log(vMap.aMap[5][5]);
+//----------------------------------
 
-//vMap.fMapSellularAutomata("B23/S45");
+
+//--------------------실행-----------
+main();
+//----------------------------------
+
+
+
