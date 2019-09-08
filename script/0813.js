75fd9e173cd3a48807f60d6cd53e8f3e7c123f68
outsung <40460655+outsung@users.noreply.github.com>
2019-09-09 00:40:09 +0900

    Add files via upload

--- script/0813.js
+++ script/0813.js
@@ -20,10 +20,10 @@ let object = {
 
 //사이즈 변경하면 실행하는 함수
 function resize() {
-  if(oGame.vStart === true){
-      
+  if(oMain.oGame.vStart === true){
+
     let hUserScreen = document.getElementById("userScreen");
-      
+
     console.log('Resizing...')
 
     //userScreen.width = window.innerWidth;
@@ -42,12 +42,12 @@ function resize() {
 
 //키 입력 시 실행하는 함수
 function keyPress(){
-  if(oPlay.vStart === true){
+  if(oMain.oPlay.vStart === true){
     let downKeyCode = event.keyCode;
-    //console.log(downKeyCode);
+    console.log(downKeyCode);
 
-    if (downKeyCode in oPlay.keyCode){
-      oPlay.keyCode[downKeyCode] = true;
+    if (downKeyCode in oMain.oPlay.keyCode){
+      oMain.oPlay.keyCode[downKeyCode] = true;
     }
     else{
       event.returnValue = false; // 브라우저 기능 키 무효화
@@ -57,12 +57,12 @@ function keyPress(){
 }
 
 function keyUp(){
-  if(oPlay.vStart === true){
+  if(oMain.oPlay.vStart === true){
     let upKeyCode = event.keyCode;
     //console.log(upKeyCode);
 
-    if (upKeyCode in oPlay.keyCode){
-      oPlay.keyCode[upKeyCode] = false;
+    if (upKeyCode in oMain.oPlay.keyCode){
+      oMain.oPlay.keyCode[upKeyCode] = false;
     }
     //console.log(keyCode);
   }
@@ -70,8 +70,8 @@ function keyUp(){
 
 
 function isKeyDown(key){
-  if(oPlay.vStart === true) return keyCode[key];   
-  else return -1;
+  if(oMain.oPlay.vStart === true) return oMain.oPlay.keyCode[key];
+  else console.log("iskeydown error!!");
 }
 
 //----------------------------------------
@@ -91,17 +91,17 @@ function getRandomBool(threshold){
 //--------------상황------------
 
 function fGame(){ // 게임 시작
-    
+
     // 마우스 위치?
     this.vStart = true;
 }
 
 
 function fPlay(){ // 게임 플레이
-  
+
     // 마우스 고정 & 마우스
     this.vStart = true;
-    
+
     //https://blog.outsider.ne.kr/322
     this.keyCode = {
     27 : false,
@@ -112,12 +112,12 @@ function fPlay(){ // 게임 플레이
     122 : false,
     123 : false
     };
-  
+
 }
 
 function fUi(){ // 게임 ui
-    
-    // 마우스 not 고정 &  
+
+    // 마우스 not 고정 &
     this.vStart = false;
 }
 
@@ -129,6 +129,9 @@ function fMap(width, height, initV){
     this.width = width;
     this.height = height;
     this.initV = initV;
+
+    this.blockSize = 20;
+
 
     // 기본값 생성
     this.getMapA = function(){
@@ -145,6 +148,7 @@ function fMap(width, height, initV){
             {
                 if(i === 0 || i === this.width + 1 ||
                   j === 0 || j === this.height + 1){
+                    //console.log("i = "+ i +" j = " + j);
                     temp.push(false);
                     i++;
                 }
@@ -153,12 +157,24 @@ function fMap(width, height, initV){
                     i++;
                 }
             }
-            console.log(temp);
+            //console.log(temp);
             res.push(temp);
             j++;
         }
         console.log("x "+this.width+" y "+this.height+" setting..");
 
+        // 크기 조정
+        /*
+        let hMap = document.getElementById("map");
+        hMap.style.width = (this.width + 2) * this.blockSize + "px";
+        hMap.style.height = (this.height + 2) * this.blockSize + "px";
+        console.log("x = " + (this.width + 2) * this.blockSize +
+                    " y = " + (this.height + 2) * this.blockSize);
+        */
+        let hMap = document.getElementById("map");
+        hMap.width = (this.width + 2) * this.blockSize;
+        hMap.height = (this.height + 2) * this.blockSize;
+        //console.log("_____________"+res);
         return res;
     };
 
@@ -168,10 +184,10 @@ function fMap(width, height, initV){
     // 세포자동화
     this.fMapSellularAutomata = function(rule){
 
-        let B = [false, false, false, false, false, false, false, false, false, false];
-        let S = [false, false, false, false, false, false, false, false, false, false];
+        let aB = [false, false, false, false, false, false, false, false, false, false];
+        let aS = [false, false, false, false, false, false, false, false, false, false];
 
-        let fGetNeighbors = function(x,y){
+        let fGetNeighbors = function(aMap,x,y){
             /*
             [1][0][1]
             [0][?][0]
@@ -191,12 +207,12 @@ function fMap(width, height, initV){
                 i = x-1;
                 while(i <= x + 1)
                 {
-                    if(j === y || i === x){
+                    if(j === y && i === x){
                         i++;
                         continue;
                     }
                     else{
-                        if(this.aMap[j][i]) vNeighbors.alive++;
+                        if(aMap[j][i]) vNeighbors.alive++;
                         else vNeighbors.dead++;
                     }
                     i++;
@@ -204,37 +220,39 @@ function fMap(width, height, initV){
                 j++;
             }
 
+            //console.log(vNeighbors.dead + vNeighbors.alive);
             if(vNeighbors.dead + vNeighbors.alive !== 8) console.log("getNei.. error roop!!");
             else return vNeighbors;
         }
 
-        
-        let ruleB = rule.split("/")[0].split;
-        let ruleS = rule.split("/")[0].split;
 
-        for(let i in ruleB)
-        {   
-            console.log(i);
-            if(typeof(i *= 1) === "number") B[i] = true;
+        let sRuleB = rule.split("/")[0].split("");
+        let sRuleS = rule.split("/")[0].split("");
+
+        //console.log(sRuleB);
+        for(let i in sRuleB)
+        {
+            //console.log(sRuleB[i]);
+            if(typeof(sRuleB[i] *= 1) === "number") aB[sRuleB[i]] = true;
             else{
-                if(i !== "B"){
+                if(sRuleB[i] !== "B"){
                     console.log("fMapSellularAutomata-[B]error-!");
                     return -1;
                 }
             }
         }
-        for(let i in ruleS)
+        for(let i in sRuleS)
         {
-            if(typeof(i *= 1) === "number") S[i] = true;
+            if(typeof(sRuleS[i] *= 1) === "number") aS[sRuleS[i]] = true;
             else{
-                if(i !== "S"){
+                if(sRuleS[i] !== "S"){
                     console.log("fMapSellularAutomata-[S]error-!");
                     return -1;
                 }
             }
         }
 
-
+        console.log(sRuleB);
         //start
         let j = 1;
         let i = 1;
@@ -245,14 +263,14 @@ function fMap(width, height, initV){
             i = 1;
             while(i < this.width + 1)
             {
-                vNeighbors = fGetNeighbors(i,j);
-
+                vNeighbors = fGetNeighbors(this.aMap,i,j);
+                //console.log(vNeighbors);
                 if(this.aMap[j][i] === true){ // Alive
-                    if(S[vNeighbors.alive] === true);
+                    if(aS[vNeighbors.alive] === true);
                     else this.aMap[j][i] = false;
                 }
                 else{ // Dead
-                    if(B[vNeighbors.alive] === true){ // B
+                    if(aB[vNeighbors.alive] === true){ // B
                         this.aMap[j][i] = true;
                     }
                 }
@@ -260,7 +278,7 @@ function fMap(width, height, initV){
 
                 i++;
             }
-            console.log(temp);
+            //console.log(temp);
             j++;
         }
 
@@ -268,51 +286,74 @@ function fMap(width, height, initV){
 
 
     this.fDrawMap = function(){
-        
+
+        console.log("B");
         // 맵 그리기
-        
+
         //-----------블럭-----------
-        let alive = function(x,y){
-            
+        let alive = function(blockSize,x,y){
+            x *= blockSize;
+            y *= blockSize;
+
             const hMap = document.getElementById("map");
             const hDraw = hMap.getContext("2d");
-            
-            
-            const width = 10;
-            const height = 10;
-            const style = "rgb(200,0,0)";
-            
-            
+
+
+            //const width = 10;
+            //const height = 10;
+            const style = "rgb(0,128,0)";
+
+
             hDraw.fillStyle = style;
-            hDraw.fillRect (x, y, width, height);
-            
-        } 
-        let dead = function(x,y){
-            
-            const hMap = document.getElementById("map");
-            const hDraw = hMap.getContext("2d");
-            
-            
-            const width = 10;
-            const height = 10;
-            const style = "rgb(200,200,0)";
-            
-            
-            hDraw.fillStyle = style;
-            hDraw.fillRect (x, y, width, height);
-            
+
+            hDraw.fillRect (x, y, blockSize, blockSize);
+            //console.log("alive draw");
+
+        }
+        let dead = function(blockSize,x,y){
+            x *= blockSize;
+            y *= blockSize;
+
+            let hMap = document.getElementById("map");
+            if(hMap.getContext){
+              let hDraw = hMap.getContext("2d");
+
+
+              //const width = 10;
+              //const height = 10;
+              const style = "rgb(80,188,223)";
+
+
+              hDraw.fillStyle = style;
+              //console.log("x = " + x);
+              //console.log("y = " + y);
+              //console.log("y = " + this.blockSize);
+              //hDraw.fillRect(0, 0, hMap.width, hMap.height);
+              hDraw.fillRect (x, y, blockSize, blockSize);
+
+            }
+            else console.log("에바인 에러");
+
         }
         //-------------------------
-        
-        
+
+        //console.log(this.aMap);
         for(let j in this.aMap){
-            for(let i in j){
-                if(this.aMap[j][i] === true) alive(i*10, j*10);
-                else dead(i*10, j*10);
+            //console.log("j = "+j);
+            for(let i in this.aMap[0]){
+                //console.log("i = "+i);
+                if(this.aMap[j][i] === true){
+                  //console.log("alive draw");
+                  alive(this.blockSize, i, j);
+                }
+                else{
+                  //console.log("dead draw");
+                  dead(this.blockSize, i, j);
+                }
             }
         }
-        
-        
+
+
     }
 }
 //-------------------------------
@@ -324,28 +365,49 @@ function fMap(width, height, initV){
 
 function main(){
 
-    let oGame = new fGame();
+    this.oGame = new fGame();
+    //console.log(this.oGame);
 
-    let oPlay = new fPlay();
+    this.oPlay = new fPlay();
 
     //let oUi = new fUi();
-    
-    let oMap = new fMap(10,10);
-    
-    oMap.fDrawMap();
+
+    this.oMap = new fMap(100,100,50);
+
+
+    //this.oMap.fDrawMap();
+    //console.log("AAAAA");
 
     //vMap.fMapSellularAutomata("B23/S45");
-    
-    let iRule = "B23/S45"
-    
-    while(isKeyDown(27) !== true){
-          
-        if(isKeyDown(65) === true){
-            oMap.fMapSellularAutomata(iRule);
-            oMap.fDrawMap();
-        }
+
+    this.updata = function(){
+      let iRule = "B23/S45"
+      let delay = 1;
+      resize();
+
+      //oMain.oMap.fMapSellularAutomata("B23/S45");
+
+      this.oMap.fDrawMap();
+
+      setInterval(maaaap(),10);
     }
-    
+
+
+}
+
+function maaaap(){
+  /*
+  console.log("z");
+  if((isKeyDown(65) === true) && (delay === 1)){
+    console.log("A");
+    this.oMap.fMapSellularAutomata(iRule);
+    this.oMap.fDrawMap();
+    delay = 0;
+  }
+  if((isKeyDown(65) !== true) && (delay === 1)){
+    delay = 1;
+  }
+  */
 }
 
 
@@ -353,8 +415,9 @@ function main(){
 
 
 //--------------------실행-----------
-main();
+let oMain = new main();
+//console.log("------------" + oMain.oPlay.vStart);
+oMain.updata();
+
+//main();
 //----------------------------------
-
-
-
